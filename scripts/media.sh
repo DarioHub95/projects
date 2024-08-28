63b9fc77e091f1adbcf4e96247150a9cc20d9e54
DarioHub95 <despositosogei@gmail.com>
2024-08-28 23:01:56 +0200

    IBiSco: Errore!

--- scripts/media.sh
+++ scripts/media.sh
@@ -8,7 +8,7 @@ while screen -ls | grep -q "[0-9]\+\.${2}"; do
     sleep 1
 done
 
-# Se la cartella non esiste, esci dallo screen
+#-------------RICHIAMA LO SCRIPT NOTIFY_ERRORS--------------------
 if [ ! -d "Dati_$2" ]; then
     ./scripts/notify_errors.sh 100 "[media.sh] La cartella Dati_$2 non esiste. Uscita dallo screen media_$2..." 
     screen -X quit
@@ -18,17 +18,15 @@ elif [ -d "Dati_$2" ] && [ "$(ls Dati_$2 | wc -l)" -eq 2 ]; then
     rm -rf "Dati_$2"
     screen -X quit
 fi
+#-------------RICHIAMA LO SCRIPT NOTIFY_ERRORS--------------------
 
 
 # Conta il numero di file nella cartella e salva le prime 16 righe del primo file di media totale
 R_tot=$(ls -1 "Dati_$2"/output* 2>/dev/null | wc -l)
 # R_tot=$(( $(ls -1 "Dati_$2" | wc -l) - 2 ))
+
 # Trova il primo file che inizia con 'output'
 output_file=$(find "Dati_$2" -maxdepth 1 -type f -name "output*" | head -n 1)
-if [ -z "$output_file" ]; then
-    echo "Nessun file di output trovato."
-    exit 1
-fi
 head -n 16 "$output_file" > "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt"
 
 # Conteggio del numero massimo di occorrenze di "-nan" negli output
@@ -82,6 +80,19 @@ mv "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt.tmp" "$
 rm temp_*.txt
 echo ""
 
+
+
+
+#-------------RICHIAMA LO SCRIPT NOTIFY_ERRORS--------------------
+if [ $(wc -l < "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt") -le 20 ]; then
+    ./scripts/notify_errors.sh 100 "[media.sh] Il file '${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt' non contiene nessun valore medio. Uscita dallo screen media_$2..." 
+    screen -X quit
+fi
+#-------------RICHIAMA LO SCRIPT NOTIFY_ERRORS--------------------
+
+
+
+
 # Processa i file di output nella directory
 echo "Sostituzione punti con virgole nel file delle medie..."
 for file in "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt"; do
@@ -92,8 +103,9 @@ done
 sed -i "1i Tasks: ${R_tot}" "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt"
 sed -i "1i Date: $(date '+%Y-%m-%d %H:%M:%S')" "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt"
 
+
+#----------------RICHIAMA LO SCRIPT NOTIFY_OK------------------------------------------
+./scripts/notify_ok.sh "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt" $start_time
+screen -X quit
 #----------------RICHIAMA LO SCRIPT NOTIFY_OK------------------------------------------
 
-./scripts/notify_ok.sh "${3}_${2}_L${4}_R${R_tot}_$(date -u -d @$start_time +'%H.%M.%S').txt" $start_time
-
-screen -X quit
