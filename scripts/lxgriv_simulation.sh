81a4dd2631f5b880f666339085d3dafa59c32d22
DarioHub95 <despositosogei@gmail.com>
2024-09-06 17:03:18 +0200

    IBiSco: Job info

--- scripts/lxgriv_simulation.sh
+++ scripts/lxgriv_simulation.sh
@@ -2,7 +2,7 @@
 
 # Comandi di DEBUG
 set -x
-trap 'sleep 1' DEBUG        # Imposta un rallentamento generale di 1 secondo prima di ogni comando
+trap 'sleep 3' DEBUG        # Imposta un rallentamento generale di 1 secondo prima di ogni comando
 
 #vars simulazione
 start_time=$(date +%s)
@@ -73,7 +73,7 @@ echo "La somma delle componenti dell'array non è 0. La somma è $sum."
 #-------------RICHIAMA LO SCRIPT NOTIFY_ERRORS--------------------
 if [ "$(ls Dati_$3 | wc -l)" -eq 2 ]; then       # Se la cartella contiene solo 2 file 
     ./scripts/notify_errors.sh 100 "[media.sh] I Job sono stati eseguiti ma la cartella Dati_$3 non contiene i dati di output. Uscita dallo screen media_$3..." 
-    screen -X quit
+    # screen -X quit
 fi
 #-----------------------------------------------------------------
 
