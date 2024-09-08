76f148804ebbe20ba5f80d969b2770079f7f3f24
DarioHub95 <despositosogei@gmail.com>
2024-09-08 11:37:47 +0200

    IBiSco: Job info

--- scripts/ibisco_simulation.sh
+++ scripts/ibisco_simulation.sh
@@ -2,7 +2,7 @@
 
 # Comandi di DEBUG
 set -x
-trap 'sleep 2' DEBUG        # Imposta un rallentamento generale di 1 secondo prima di ogni comando
+trap 'sleep 3' DEBUG        # Imposta un rallentamento generale di 1 secondo prima di ogni comando
 
 #vars simulazione
 start_time=$(date +%s)
