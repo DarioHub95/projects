6c3568aae7dcb1b69e55705fa107b2453b32c294
DarioHub95 <despositosogei@gmail.com>
2024-09-19 16:03:18 +0200

    IBiSco: Job info

--- ibisco-run.sh
+++ ibisco-run.sh
@@ -4,9 +4,7 @@ echo ""
 GREEN='\033[0;32m'
 NC='\033[0m'
 # script_path="scripts/ibisco_simulation.sh"
-script_path="scripts/new_project_simulation.sh"
-# script_path="scripts/lxgriv_simulation.sh"
-
+script_path="scripts/new_ibisco_simulation.sh"
 
 echo -e "${GREEN}[TASK 0 - PULIZIA AMBIENTE]-----------------------------------------------|${NC}"
 echo ""
